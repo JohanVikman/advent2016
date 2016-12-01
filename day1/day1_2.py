b0ba0e27469693596888e31ffeadd5b596f9ceeb
Johan Vikman <jvikman@tail-f.com>
2016-12-01 18:35:04 +0100

    Fixed day1 2nd problem

--- day1/day1_2.py
+++ day1/day1_2.py
@@ -1,12 +1,11 @@
-
 def run():
     input = open("input.txt", 'r')
     lines = file.read(input)
     liness = lines.replace("\n", " ").split(", ")
     ws = ["n","e","s","w"]
     facing = 0
-    x = 0
-    y = 0
+    x = x0 = 0
+    y = y0 = 0
     visited = {}
     for instruction in liness:
         if instruction[0] == "L":
@@ -14,6 +13,8 @@ def run():
         elif instruction[0] == "R":
             facing += 1
         wsi = ws[facing % 4]
+        y0 = y
+        x0 = x
         if wsi == "n":
             y += int(instruction[1:])
         elif wsi == "s":
@@ -22,16 +23,46 @@ def run():
             x -= int(instruction[1:])
         elif wsi == "e":
             x += int(instruction[1:])
-        print("Intr : {} wsi : {} y : {} x : {}".format(instruction, wsi, y, x))
-        loc = "{},{}".format(x,y)
-        if visited.has_key(loc):
-            print("Found it at {} {} shortest here {}".format(y,x, abs(y) + abs(x)))
-            return
-        visited[loc] = True
+        locs = make_passed_locs(y0, y, x0, x)
+        for loc in locs:
+            lockey="{},{}".format(loc[0],loc[1])
+            if visited.has_key(lockey):
+                print("Found it at {} shortest here {}".format(loc,
+                                                               abs(loc[0]) + abs(loc[1])
+                                                               )
+                      )
+                return
+            # Otherwise keep adding them until we found it
+            visited[lockey] = True
 
-    print("Final location y : {} x : {}".format(y, x))
+    print("Final location without any crossings y : {} x : {}".format(y, x))
     shortestway = abs(y) + abs(x)
-    print("Shortest way (no blocks) : {}".format(shortestway))
+    print("Shortest way: {}".format(shortestway))
+
+def make_passed_locs(y0, y, x0, x):
+    yrange = make_range(y0, y)
+    xrange = make_range(x0, x)
+    locs = []
+    if len(yrange) > 1:
+        for yi in yrange:
+            locs.append([x,yi])
+    else:
+        for xi in xrange:
+            locs.append([xi,y])
+    return locs
+
+
+def make_range(pos0, pos):
+    posrange = []
+    if pos0 != pos:
+        if pos0 < pos:
+            posrange = range(pos,pos0, -1)
+        else:
+            posrange = range(pos,pos0)
+    else:
+        posrange = [pos]
+    return posrange
+
 
 if __name__ == '__main__':
     run()
